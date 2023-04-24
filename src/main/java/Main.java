32af7a15d621dce6b3f1fa366193ff919c8ebe22
c-is-for-chi <129114186+c-is-for-chi@users.noreply.github.com>
2023-04-23 22:32:06 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        Cat coco = new Cat("coco", 14);
-        Dog max = new Dog("max", 5, "Peng");
-        Rabbit rabbit = new Rabbit("snowball", 3, "black");
-        Toy robot = new Toy("curious George");
+        Cat coco = new Cat("Coco", 14);
+        // coco.setName("Coco II");
+        // coco.play();
+        // System.out.println("There's a cat called " + coco.getName() + " on the table! \n");
 
-        Action[] animals = new Action[]{coco, max, rabbit, robot};
-        for (Action friend : animals) {
-            friend.play();
+        Dog max = new Dog ("Max", 5, "Peng");
+        // max.play();
+
+        Rabbit snowWhite = new Rabbit("Snowball", "red");
+
+        Toy curiousGeorge = new Toy("ABC corp");
+
+        // curiousGeorge.play();
+
+        /*
+        Animal[] animals = new Animal[4];
+        animals[0] = coco;
+        animals[1] = max;
+        animals[2] = snowWhite;
+        animals[3] = curiousGeorge;
+
+        for (Animal animal : animals) {
+            animal.play();
+        }
+        */
+
+        Toy toy = new Toy("Boytoy");
+
+        //Action[] homeFriend = new Action[4];
+        //homeFriend[0] = coco;
+        //homeFriend[1] = max;
+        //homeFriend[2] = snowWhite;
+        //homeFriend[3] = curiousGeorge;
+
+        Action[] homeFriend = new Action[1];
+        homeFriend[0] = toy;
+
+        for (Action friend : homeFriend) {
+            friend.play(toy);
         }
     }
 }
