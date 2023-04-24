fb81dd8c5e3f75f6119063303a2b3f7feaed34b5
c-is-for-chi <129114186+c-is-for-chi@users.noreply.github.com>
2023-04-23 22:31:46 -0700

    Update Dog.java

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,13 +1,19 @@
-public class Dog extends Animal {
+public class Dog extends Animal{
     private String owner;
 
-    public Dog(String name, int age, String owner) {
-        super(name, age);
+    public Dog (String name, int age, String owner) {
+        // super(name, age);
+        // ^ if you add that, you can delete the next two lines
+        this.name = name;
+        this.age = age;
         this.owner = owner;
     }
 
-    @Override
-    public void play() {
-        System.out.println("DOG " + name + " is chasing its tail");
+    public void setName() {
+        this.name = name;
+    }
+
+    public void play(Toy toy) {
+        System.out.println("DOG " + name + " is chasing it's tail!");
     }
 }
