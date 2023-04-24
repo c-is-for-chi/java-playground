8519bef0a4a96400ac4122fba36c5f16191ee7ee
c-is-for-chi <129114186+c-is-for-chi@users.noreply.github.com>
2023-04-23 22:32:27 -0700

    Update Rabbit.java

--- src/main/java/Rabbit.java
+++ src/main/java/Rabbit.java
@@ -1,13 +1,12 @@
 public class Rabbit extends Animal {
     private String eyeColor;
 
-    public Rabbit(String name, int age, String eyeColor) {
-        super(name, age);
+    public Rabbit(String name, String eyeColor) {
+        this.name = name;
         this.eyeColor = eyeColor;
     }
 
-    @Override
-    public void play() {
-        System.out.println("Rabbit " + name + " is idling");
+    public void play(Toy toy) {
+        System.out.println("RABBIT: " + name + " is... being still?");
     }
 }
